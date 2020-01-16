7d7aa3cbed4b67a5013d45b0583edf0dfe6bd554
Gaserd <gaserd123@gmail.com>
2020-01-16 16:38:11 +0300

    some changes in LIVE

--- live.js
+++ live.js
@@ -3,7 +3,7 @@ let MATCHES = []
 
 function get_live(id) {
     return new Promise((resolve, reject) => {
-        fetch('https://cors-anywhere.herokuapp.com/https://www.trackdota.com/data/game/' + id + '/live.json')
+        fetch('https://cors-anywhere.herokuapp.com/https://www.trackdota.com/data/game/' + id + '/core.json')
             .then(res => res.json())
             .then(data => {
                 resolve(data)
@@ -43,36 +43,76 @@ function render_table() {
     let container = document.querySelector('.container')
     let table = ''
 
-    for (let i = 0; i < data.length; i++) {
-
-        table += '<table><tbody>'
+    table += '<table><tbody>'
 
         table += `<thead>
-                <td>Start time</td>
+                <td>ID</td>
+                <td>Tier</td>
                 <td>First Team</td>
                 <td>Second Team</td>
                 <td>Scores</td>
+                <td>Dota Picker</td>
             </thead>`
-        let time = new Date(data[i].activate_time * 1000)
-        let str_time = time.toLocaleString()
+
+    for (let i = 0; i < data.length; i++) {
 
         table += `<tr>`
-        table += `<td>${str_time}</td>`
-        table += `<td>${data[i].team_name_radiant} <span class='radiant net_gold' data-id='${data[i].match_id}'></span></td>`
+        table += `<td>${data[i].match_id}</td>`
+        table += `<td class='tier' data-id='${data[i].match_id}'></td>`
         table += `<td>${data[i].team_name_dire} <span class='dire net_gold' data-id='${data[i].match_id}'></span></td>`
-        table += `<td>${data[i].radiant_score}:${data[i].dire_score}</td>`
+        table += `<td>${data[i].team_name_radiant} <span class='radiant net_gold' data-id='${data[i].match_id}'></span></td>`
+        table += `<td>${data[i].dire_score}:${data[i].radiant_score}</td>`
+        table += `<td class='url_dota_picker' data-id='${data[i].match_id}'></td>`
         table += '</tr>'
-        table += '<tbody></table>'
-        table += `<div class='canvas-chart' style="position: relative; height:40vh; width:80vw">
-            <canvas class='live' style="height:40vh; width:80vw" data-id='${data[i].match_id}'></canvas>
-        </div>`
     }
+    table += '<tbody></table>'
     container.innerHTML = table
 }
 
 function delete_table() {
     let container = document.querySelector('.container')
     container.innerHTML = ''
+}
+
+
+function get_url_dotapicker(data) {
+    const url_dota_picker = 'https://dotapicker.com/herocounter?language=ru-ru#!/'
+    let dire_picks = []
+    let radiant_picks = []
+
+    let getHeroesName = (id) => {
+        let name = null
+        for (let i = 0; i < HEROES.length; i++) {
+            if (HEROES[i].id == id) {
+                name = HEROES[i].localized_name.replace(' ', '_')
+            }
+        }
+        return name
+    }
+
+    if (data.dire_picks.length == 5 && data.radiant_picks.length == 5) {
+        dire_picks = data.dire_picks
+        radiant_picks = data.radiant_picks
+
+        let string_url_dire_picks = ''
+        let string_url_radiant_picks = ''
+        if (dire_picks.length == 5 && radiant_picks.length == 5) {
+            for (let i = 0; i < dire_picks.length; i++) {
+                if (i == 0) {
+                    string_url_dire_picks += `T_${getHeroesName(dire_picks[i].hero_id)}`
+                    string_url_radiant_picks += `/E_${getHeroesName(radiant_picks[i].hero_id)}`
+                } else {
+                    string_url_radiant_picks += `/E_${getHeroesName(radiant_picks[i].hero_id)}`
+                    string_url_dire_picks += `/T_${getHeroesName(dire_picks[i].hero_id)}`
+                }
+            }
+        }
+
+        return `${url_dota_picker}${string_url_dire_picks}${string_url_radiant_picks}`
+
+    } else {
+        return null
+    }
 }
 
 function main() {
@@ -84,47 +124,11 @@ function main() {
             for (let i = 0; i < MATCHES.length; i++) {
                 get_live(MATCHES[i].match_id)
                     .then(data => {
-                        
                         let match_id = MATCHES[i].match_id
-                        let status = data.status
-                        let gold_difference = []
-                        let time = []
-                        for (let i = 0; i < data.radiant.stats.net_gold.length; i++) {
-                            time.push(i + ':00')
-                            gold_difference.push(
-                                sum_gold(data.radiant.stats.net_gold.slice(0, i + 1)) - sum_gold(data.dire.stats.net_gold.slice(0, i + 1))
-                            )
-                        }
+                        let url_dota_picker = get_url_dotapicker(data)
 
-                        document.querySelector('span.dire[data-id="'+match_id+'"]').innerHTML = ' | ' + data.dire.stats.net_gold[data.dire.stats.net_gold.length - 1]
-                        document.querySelector('span.radiant[data-id="'+match_id+'"]').innerHTML = ' | ' + data.radiant.stats.net_gold[data.radiant.stats.net_gold.length - 1]
-
-                        let chart_string = match_id + ' | GOLD DIFFERENCE | '
-                        if (status == 1) {
-                            chart_string += 'Wait game start'
-                        } else if (status == 3) {
-                            chart_string += 'Game start'
-                        } else if (status == 4) {
-                            chart_string += 'Game Over'
-                        }
-
-                        var ctx = document.querySelector('.live[data-id="' + MATCHES[i].match_id + '"').getContext('2d');
-                        var chart = new Chart(ctx, {
-                            type: 'line',
-                            data: {
-                                labels: time,
-                                datasets: [{
-                                    label: chart_string,
-                                    backgroundColor: 'rgb(205, 220, 57)',
-                                    borderColor: 'rrgb(205, 220, 57)',
-                                    data: gold_difference
-                                }]
-                            },
-                            options: {
-                                responsive : true,
-                                animation: false
-                            }
-                        });
+                        document.querySelector('.url_dota_picker[data-id="' + match_id + '"').innerHTML = '<a href='+ url_dota_picker +' target="blank">CLICK</a>'
+                        document.querySelector('.tier[data-id="'+match_id+'"').innerHTML = data.league.tier
                     })
             }
         })
@@ -132,6 +136,6 @@ function main() {
 
 main()
 
-setInterval(function() {
+setInterval(function () {
     main()
 }, 15000)
